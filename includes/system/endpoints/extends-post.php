8b8e148f56976c03992f62f201d2b09b2c09f800
Oleg Podmarkov <oleg.podmarkov@prosvit.design>
2019-07-02 12:12:04 +0300

    extemded categories

--- includes/system/endpoints/extends-post.php
+++ includes/system/endpoints/extends-post.php
@@ -10,6 +10,7 @@ class Extends_Post extends \WP_REST_Controller
   public function __construct()
   {
     add_action( 'rest_api_init', [ $this, 'add_sidebar_settings' ], 10 );
+    add_action( 'rest_api_init', [ $this, 'add_extended_categories' ], 10 );
   }
 
   public function add_sidebar_settings()
@@ -22,9 +23,18 @@ class Extends_Post extends \WP_REST_Controller
 	  );
   }
 
+  public function add_extended_categories()
+  {
+    register_rest_field( 'post',
+      'extended_categories',
+      [
+        'get_callback'    => [ $this, 'get_extended_categories' ],
+      ]
+	  );
+  }
+
   public function get_sidebar_settings( $object, $field_name, $request )
   {
-
     $id = $object['id'];
 
     if ( is_wp_error( $id ) ) {
@@ -38,6 +48,38 @@ class Extends_Post extends \WP_REST_Controller
     }
 
     return $data;
+  }
+
+  public function get_extended_categories( $object, $field_name, $request )
+  {
+    $id = $object['id'];
+
+    if ( is_wp_error( $id ) ) {
+      return $id;
+    }
+
+    $taxonomies = get_taxonomies( [ 'public' => true ], 'names' );
+    $post_terms = wp_get_object_terms( $id, array_values( $taxonomies ) );
+
+    $datas = [];
+
+    if( $post_terms && ! is_wp_error( $post_terms ) ){
+      foreach ($post_terms as $term ) {
+        $datas[] = [
+          'term_id'     => $term->term_id,
+          'name'        => $term->name,
+          'taxonomy'    => $term->taxonomy,
+          'description' => $term->description,
+          'count'       => $term->count,
+          'slug'        => str_replace( get_site_url(), '', get_term_link( $term ) )
+        ];
+      }
+    }else{
+      return false;
+    }
+
+    return $datas;
+
   }
 
 }

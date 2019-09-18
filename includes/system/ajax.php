46d219a61b2096a9342581e065d4738a3872946b
Oleg Podmarkov <oleg.podmarkov@prosvit.design>
2019-09-18 17:50:03 +0300

    v4

--- includes/system/ajax.php
+++ includes/system/ajax.php
@@ -6,54 +6,149 @@ class Ajax{
 
   public function __construct()
   {
-    add_action( 'wp_ajax_nopriv_initCheckoutFields', [ $this, 'initCheckoutFields' ] );
-    add_action( 'wp_ajax_initCheckoutFields', [ $this, 'initCheckoutFields' ] );
+    add_action( 'wp_ajax_updateTotals', [ $this, 'updateTotals' ] );
+    add_action( 'wp_ajax_nopriv_updateTotals', [ $this, 'updateTotals' ] );
+
+    add_action( 'wp_ajax_nopriv_processCheckoutFields', [ $this, 'processCheckoutFields' ] );
+    add_action( 'wp_ajax_processCheckoutFields', [ $this, 'processCheckoutFields' ] );
   }
 
-  public function initCheckoutFields()
+  public function updateTotals()
   {
-    $fieldsets = WC()->checkout->get_checkout_fields();
-    $usersets = [];
+    if ( WC()->cart->is_empty() && ! is_customize_preview() ) {
+      wp_send_json_error( [ 'code' => 110, 'message' => 'Empty cart' ], 405 );
+    }
 
-    $user = is_user_logged_in() ? WC()->customer : false;
+    WC()->session->set( 'chosen_shipping_methods', empty( $_POST['shipping_method'] ) ? '' : [$_POST['shipping_method']] );
+    WC()->session->set( 'chosen_payment_method', empty( $_POST['payment_method'] ) ? '' : $_POST['payment_method'] );
+    WC()->customer->set_props(
+      array(
+        'billing_country'   => isset( $_POST['billing_country'] ) ? wp_unslash( $_POST['billing_country'] ) : null,
+        'billing_state'     => isset( $_POST['billing_state'] ) ? wp_unslash( $_POST['billing_state'] ) : null,
+        'billing_postcode'  => isset( $_POST['billing_postcode'] ) ? wp_unslash( $_POST['billing_postcode'] ) : null,
+        'billing_city'      => isset( $_POST['billing_city'] ) ? wp_unslash( $_POST['billing_city'] ) : null,
+        'billing_address_1' => isset( $_POST['billing_address_1'] ) ? wp_unslash( $_POST['billing_address_1'] ) : null,
+        'billing_address_2' => isset( $_POST['billing_address_2'] ) ? wp_unslash( $_POST['billing_address_2'] ) : null,
+        'billing_phone' => isset( $_POST['billing_phone'] ) ? wp_unslash( $_POST['billing_phone'] ) : null,
+      )
+    );
+
+    if ( wc_ship_to_billing_address_only() ) {
+      WC()->customer->set_props(
+        array(
+          'shipping_country'   => isset( $_POST['billing_country'] ) ? wp_unslash( $_POST['billing_country'] ) : null,
+          'shipping_state'     => isset( $_POST['billing_state'] ) ? wp_unslash( $_POST['billing_state'] ) : null,
+          'shipping_postcode'  => isset( $_POST['billing_postcode'] ) ? wp_unslash( $_POST['billing_postcode'] ) : null,
+          'shipping_city'      => isset( $_POST['billing_city'] ) ? wp_unslash( $_POST['billing_city'] ) : null,
+          'shipping_address_1' => isset( $_POST['billing_address_1'] ) ? wp_unslash( $_POST['billing_address_1'] ) : null,
+          'shipping_address_2' => isset( $_POST['billing_address_2'] ) ? wp_unslash( $_POST['billing_address_2'] ) : null,
+        )
+      );
+    } else {
+      WC()->customer->set_props(
+        array(
+          'shipping_country'   => isset( $_POST['shipping_country'] ) ? wp_unslash( $_POST['shipping_country'] ) : null,
+          'shipping_state'     => isset( $_POST['shipping_state'] ) ? wp_unslash( $_POST['shipping_state'] ) : null,
+          'shipping_postcode'  => isset( $_POST['shipping_postcode'] ) ? wp_unslash( $_POST['shipping_postcode'] ) : null,
+          'shipping_city'      => isset( $_POST['shipping_city'] ) ? wp_unslash( $_POST['shipping_city'] ) : null,
+          'shipping_address_1' => isset( $_POST['shipping_address_1'] ) ? wp_unslash( $_POST['shipping_address_1'] ) : null,
+          'shipping_address_2' => isset( $_POST['shipping_address_2'] ) ? wp_unslash( $_POST['shipping_address_2'] ) : null,
+        )
+      );
+    }
+
+    WC()->customer->save();
+    WC()->cart->calculate_shipping();
+    WC()->cart->calculate_totals();
+    
+    wp_send_json_success( [ 'message' => 'Datas updated' ], 200 );
+  }
+
+  public function processCheckoutFields()
+  {
+    $user = false;
+    $usersets = [];
+    $usersets['ship_to_different_address'] = false;
+    $usersets['shipping_method'] = false;
+    $usersets['payment_method'] = false;
+
+    if ( $this->meybe_post_request() ) {
+      $user = $this->get_posted_user_data();
+      $usersets['ship_to_different_address'] = $user['ship_to_different_address'];
+    } elseif ( is_user_logged_in() ) {
+      $user = WC()->customer;
+    }
+
+    $fieldsets = WC()->checkout->get_checkout_fields();
 
     foreach ( $fieldsets as $section => $fields ) {
+      $validate_fieldset = false;
+
+      if ( $user ) {
+        if ( ! is_a( $user, 'WC_Customer' ) ) {
+          $validate_fieldset = true;
+
+          if ( $this->maybe_skip_fieldset( $section, $user ) ) {
+            $validate_fieldset = false;
+          }
+        }
+      }
+
+
       foreach ( $fields as $key => $field ) {
         $fieldsets[$section][$key]['section'] = $section;
         $fieldsets[$section][$key]['name'] = $key;
 
-        switch ( $field['type'] ) {
-          case 'country':
-            $fieldsets[$section][$key]['field_type'] = 'country';
-            $fieldsets[$section][$key]['values'] = WC()->countries->get_countries();
+        if ( $user && $this->meybe_post_request() ) {
+          if ( $error = $this->validate_field( $validate_fieldset, $key, $field, $section, $user ) ) {
+            $fieldsets[$section][$key]['validation'] = $error;
+          }
+        }
 
-            if ( $user ) {
+        if ( isset( $field['type'] ) && $field['type'] === 'country' ) {
+          $fieldsets[$section][$key]['field_type'] = 'select';
+          $fieldsets[$section][$key]['values'] = WC()->countries->get_countries();
+
+          if ( $user ) {
+            if ( is_a( $user, 'WC_Customer' ) ) {
               $usersets[$key] = $user->get_billing_country();
             } else {
-              $usersets[$key] = WC()->countries->get_base_country();
+              $usersets[$key] = isset( $user[$key] ) ? $user[$key] : '';
             }
-            break;
-          
-          case 'state':
-            $fieldsets[$section][$key]['field_type'] = 'state';
-            $fieldsets[$section][$key]['values'] = WC()->countries->get_states( $user ? $user->get_billing_country() : WC()->countries->get_base_country() );
+          } else {
+            $usersets[$key] = WC()->countries->get_base_country();
+          }
+        } elseif( isset( $field['type'] ) && $field['type'] === 'state' ) {
+          $fieldsets[$section][$key]['field_type'] = 'state';
+          $fieldsets[$section][$key]['values'] = WC()->countries->get_states( $user ? $user->get_billing_country() : WC()->countries->get_base_country() );
 
-            if ( $user ) {
+          if ( $user ) {
+            if ( is_a( $user, 'WC_Customer' ) ) {
               $usersets[$key] = $user->get_billing_state();
             } else {
-              $usersets[$key] = WC()->countries->get_base_state();
+              $usersets[$key] = isset( $user[$key] ) ? $user[$key] : '';
             }
-            break;
-          
-          case 'textarea':
-            $fieldsets[$section][$key]['field_type'] = 'textarea';
-            $fieldsets[$section][$key]['filled'] = '';
-            break;
+          } else {
+            $usersets[$key] = WC()->countries->get_base_state();
+          }
+        } elseif( isset( $field['type'] ) && $field['type'] === 'textarea' ) {
+          $fieldsets[$section][$key]['field_type'] = 'textarea';
+          $fieldsets[$section][$key]['filled'] = '';
 
-          default:
-            $fieldsets[$section][$key]['field_type'] = 'input';
+          if ( $user ) {
+            if ( is_a( $user, 'WC_Customer' ) ) {
+              $usersets[$key] = '';
+            } else {
+              $usersets[$key] = isset( $user[$key] ) ? $user[$key] : '';
+            }
+          } else {
+            $usersets[$key] = '';
+          }
+        } else {
+          $fieldsets[$section][$key]['field_type'] = 'input';
 
-            if ( $user ) {
+          if ( $user ) {
+            if ( is_a( $user, 'WC_Customer' ) ) {
               if ( $section === 'billing' ) {
                 $data = $user->get_billing();
               } elseif( $section === 'shipping' ) {
@@ -63,17 +158,223 @@ class Ajax{
               }
 
               $field_key = str_replace( $section . '_', '', $key );
-              $usersets[$key] = $data[$field_key];
+              $usersets[$key] = !empty( $data[$field_key] ) ? $data[$field_key] : '';
             } else {
-              $usersets[$key] = '';
+              $usersets[$key] = isset( $user[$key] ) ? $user[$key] : '';
             }
-            break;
+          } else {
+            $usersets[$key] = '';
+          }
         }
 
       }
 
     }
 
-    wp_send_json_success( [ 'fields' => $fieldsets, 'user' => $usersets ], 200 );
+    wp_send_json_success( [
+      'fields' => $fieldsets,
+      'user' => $usersets,
+      'shipping' => $this->get_shipping_methods(),
+      'payment' => $this->get_payment_methods()
+    ], 200 );
   }
+
+
+
+  private function get_posted_user_data()
+  {
+    $skipped = [];
+    $data = [
+      'ship_to_different_address' => ! empty( $_POST['ship_to_different_address'] ) && ! wc_ship_to_billing_address_only(),
+    ];
+
+    foreach ( WC()->checkout->get_checkout_fields() as $fieldset_key => $fieldset ) {
+      if ( $this->maybe_skip_fieldset( $fieldset_key, $data ) ) {
+        $skipped[] = $fieldset_key;
+        continue;
+      }
+
+      foreach ( $fieldset as $key => $field ) {
+        $type = sanitize_title( isset( $field['type'] ) ? $field['type'] : 'text' );
+
+        switch ( $type ) {
+            case 'checkbox':
+                $value = isset( $_POST[ $key ] ) ? 1 : '';
+                break;
+            case 'multiselect':
+                $value = isset( $_POST[ $key ] ) ? implode( ', ', wc_clean( wp_unslash( $_POST[ $key ] ) ) ) : '';
+                break;
+            case 'textarea':
+                $value = isset( $_POST[ $key ] ) ? wc_sanitize_textarea( wp_unslash( $_POST[ $key ] ) ) : '';
+                break;
+            case 'password':
+                $value = isset( $_POST[ $key ] ) ? wp_unslash( $_POST[ $key ] ) : '';
+                break;
+            default:
+                $value = isset( $_POST[ $key ] ) ? wc_clean( wp_unslash( $_POST[ $key ] ) ) : '';
+                break;
+        }
+
+        $data[ $key ] = $value;
+      }
+    }
+
+    if ( in_array( 'shipping', $skipped, true ) && ( WC()->cart->needs_shipping_address() || wc_ship_to_billing_address_only() ) ) {
+      foreach ( $this->get_checkout_fields( 'shipping' ) as $key => $field ) {
+          $data[ $key ] = isset( $data[ 'billing_' . substr( $key, 9 ) ] ) ? $data[ 'billing_' . substr( $key, 9 ) ] : '';
+      }
+    }
+
+    return $data;
+  }
+
+  private function get_shipping_methods()
+  {
+    $data = [];
+
+    WC()->shipping->calculate_shipping( WC()->cart->get_shipping_packages() );
+
+    $shipping_methods = WC()->shipping->get_packages();
+
+    foreach( $shipping_methods[0]['rates'] as $id => $shipping_method ){
+      $data[] = [
+        'method_id' => $shipping_method->method_id,
+        'type'      => $shipping_method->method_id,
+        'id'        => $shipping_method->id,
+        'name'      => $shipping_method->label,
+        'price'     => wc_price( $shipping_method->cost ),
+        'taxes'     => $shipping_method->taxes,
+      ];
+    }
+  
+    if( !empty( $data ) ){
+      return $data;
+    }else{
+      return false;
+    }
+  }
+
+  private function get_payment_methods()
+  {
+    if( !WC()->cart->is_empty() ){
+      $data = [];
+      
+      foreach( WC()->payment_gateways->get_available_payment_gateways() as $key => $gateway ){
+        $data[] = [
+          'id'                => $gateway->id,
+          'order_button_text' => $gateway->order_button_text,
+          'title'             => $gateway->get_title(),
+          'description'       => $gateway->get_description(),
+        ];
+      }
+
+      return $data;
+    }else{
+      return false;
+    }
+  }
+
+  private function get_posted_methods()
+  {
+    if ( ! isset( $_REQUEST['posted_data'] ) ) {
+      return false;
+    }
+
+    $data = [
+      'payment_method'                     => isset( $_POST['payment_method'] ) ? wc_clean( wp_unslash( $_POST['payment_method'] ) ) : '',
+      'shipping_method'                    => isset( $_POST['shipping_method'] ) ? wc_clean( wp_unslash( $_POST['shipping_method'] ) ) : '',
+      'woocommerce_checkout_update_totals' => isset( $_POST['woocommerce_checkout_update_totals'] ),
+    ];
+
+    return $data;
+  }
+
+  private function validate_field( $validate_fieldset, $key, $field, $section, $user )
+  {
+    $required    = ! empty( $field['required'] );
+    $format      = array_filter( isset( $field['validate'] ) ? (array) $field['validate'] : [] );
+    $field_label = isset( $field['label'] ) ? $field['label'] : '';
+
+    switch ( $section ) {
+      case 'shipping':
+          $field_label = sprintf( __( 'Shipping %s', 'woocommerce' ), $field_label );
+          break;
+      case 'billing':
+          $field_label = sprintf( __( 'Billing %s', 'woocommerce' ), $field_label );
+          break;
+    }
+
+    if ( in_array( 'postcode', $format, true ) ) {
+      $country      = isset( $user[ $section . '_country' ] ) ? $user[ $section . '_country' ] : WC()->customer->{"get_{$section}_country"}();
+      $user[ $key ] = wc_format_postcode( $user[ $key ], $country );
+
+      if ( $validate_fieldset && '' !== $user[ $key ] && ! \WC_Validation::is_postcode( $user[ $key ], $country ) ) {
+          switch ( $country ) {
+              case 'IE':
+                  $postcode_validation_notice = sprintf( __( '%1$s is not valid. You can look up the correct Eircode <a target="_blank" href="%2$s">here</a>.', 'woocommerce' ), '<strong>' . esc_html( $field_label ) . '</strong>', 'https://finder.eircode.ie' );
+                  break;
+              default:
+                  $postcode_validation_notice = sprintf( __( '%s is not a valid postcode / ZIP.', 'woocommerce' ), '<strong>' . esc_html( $field_label ) . '</strong>' );
+          }
+          return $postcode_validation_notice;
+      }
+    }
+
+    if ( in_array( 'phone', $format, true ) ) {
+      if ( $validate_fieldset && '' !== $user[ $key ] && ! \WC_Validation::is_phone( $user[ $key ] ) ) {
+        return sprintf( __( '%s is not a valid phone number.', 'woocommerce' ), '<strong>' . esc_html( $field_label ) . '</strong>' );
+      }
+    }
+
+    if ( in_array( 'email', $format, true ) && '' !== $user[ $key ] ) {
+      $email_is_valid = is_email( $user[ $key ] );
+      $user[ $key ]   = sanitize_email( $user[ $key ] );
+
+      if ( $validate_fieldset && ! $email_is_valid ) {
+        return sprintf( __( '%s is not a valid email address.', 'woocommerce' ), '<strong>' . esc_html( $field_label ) . '</strong>' );
+      }
+    }
+
+    if ( '' !== $user[ $key ] && in_array( 'state', $format, true ) ) {
+      $country      = isset( $user[ $section . '_country' ] ) ? $user[ $section . '_country' ] : WC()->customer->{"get_{$section}_country"}();
+      $valid_states = WC()->countries->get_states( $country );
+
+      if ( ! empty( $valid_states ) && is_array( $valid_states ) && count( $valid_states ) > 0 ) {
+          $valid_state_values = array_map( 'wc_strtoupper', array_flip( array_map( 'wc_strtoupper', $valid_states ) ) );
+          $user[ $key ]       = wc_strtoupper( $user[ $key ] );
+
+          if ( isset( $valid_state_values[ $user[ $key ] ] ) ) {
+              $user[ $key ] = $valid_state_values[ $user[ $key ] ];
+          }
+
+          if ( $validate_fieldset && ! in_array( $user[ $key ], $valid_state_values, true ) ) {
+            return sprintf( __( '%1$s is not valid. Please enter one of the following: %2$s', 'woocommerce' ), '<strong>' . esc_html( $field_label ) . '</strong>', implode( ', ', $valid_states ) );
+          }
+      }
+    }
+
+    if ( $validate_fieldset && $required && '' === $user[ $key ] ) {
+      return sprintf( __( '%s is a required field.', 'woocommerce' ), '<strong>' . esc_html( $field_label ) . '</strong>' );
+    }
+  }
+
+
+  protected function meybe_post_request()
+  {
+    if ( ! isset( $_REQUEST['posted_data'] ) ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  protected function maybe_skip_fieldset( $fieldset_key, $data )
+  {
+    if ( 'shipping' === $fieldset_key && ( ! $data['ship_to_different_address'] || ! WC()->cart->needs_shipping_address() ) ) {
+        return true;
+    }
+
+    return false;
+  }
+
 }
--- includes/system/ajax/cart.php
+++ includes/system/ajax/cart.php
@@ -15,26 +15,11 @@ class Cart{
     add_action( 'wp_ajax_removeCoupon', [ $this, 'remove_coupon' ] );
     add_action( 'wp_ajax_nopriv_removeCoupon', [ $this, 'remove_coupon' ] );
 
-    add_action( 'wp_ajax_userUpdateCheckoutData', [ $this, 'update_checkout_user_datas' ] );
-    add_action( 'wp_ajax_nopriv_userUpdateCheckoutData', [ $this, 'update_checkout_user_datas' ] );
-
-    add_action( 'wp_ajax_userGetCheckoutData', [ $this, 'get_checkout_user_datas' ] );
-    add_action( 'wp_ajax_nopriv_userGetCheckoutData', [ $this, 'get_checkout_user_datas' ] );
-
-    add_action( 'wp_ajax_getShippingMethods', [ $this, 'get_shipping_methods' ] );
-    add_action( 'wp_ajax_nopriv_getShippingMethods', [ $this, 'get_shipping_methods' ] );
-
-    add_action( 'wp_ajax_getPaymentMethods', [ $this, 'get_payment_methods' ] );
-    add_action( 'wp_ajax_nopriv_getPaymentMethods', [ $this, 'get_payment_methods' ] );
-
     add_action( 'wp_ajax_getCartProducts', [ $this, 'get_cart_products' ] );
     add_action( 'wp_ajax_nopriv_getCartProducts', [ $this, 'get_cart_products' ] );
 
     add_action( 'wp_ajax_deleteCartProduct', [ $this, 'delete_cart_product' ] );
     add_action( 'wp_ajax_nopriv_deleteCartProduct', [ $this, 'delete_cart_product' ] );
-
-    add_action( 'wp_ajax_nopriv_getCountries', [ $this, 'get_countries' ] );
-    add_action( 'wp_ajax_getCountries', [ $this, 'get_countries' ] );
 
     add_action( 'wp_ajax_nopriv_getStates', [ $this, 'get_states' ] );
     add_action( 'wp_ajax_getStates', [ $this, 'get_states' ] );
@@ -85,112 +70,6 @@ class Cart{
     }
   }
 
-  public function update_checkout_user_datas()
-  {
-    if ( WC()->cart->is_empty() && ! isset( $_POST['name'] ) && ! isset( $_POST['value'] ) ) {
-      wp_send_json_error( [ 'code' => 110, 'message' => 'Empty cart' ], 405 );
-    }
-
-    $allowed = [
-      'billing_email',
-      'billing_first_name',
-      'billing_last_name',
-      'billing_phone',
-      'billing_country',
-      'billing_state',
-      'billing_postcode',
-      'billing_address_1',
-      'shipping_method',
-      'payment_method'
-    ];
-
-    $name = wc_clean( wp_unslash( $_POST['name'] ) );
-    $value = wc_clean( wp_unslash( $_POST['value'] ) );
-
-    if( ! in_array( $name, $allowed ) ){
-      wp_send_json_error( [ 'code' => 401, 'message' => 'Not allowed method' ], 405 );
-    }
-
-    if( $name == 'shipping_method' || $name == 'payment_method' ){
-      $posted_shipping_methods = ( $name == 'shipping_method' ? [$value] : [] );
-
-      WC()->session->set( 'chosen_shipping_methods', $posted_shipping_methods );
-      WC()->session->set( 'chosen_payment_method', ( $name == 'payment_method' ? $value : '' ) );
-    }else{
-      WC()->customer->set_props( [ $name => $value ] );
-    }
-
-    WC()->customer->save();
-    WC()->cart->calculate_shipping();
-		WC()->cart->calculate_totals();
-    
-    wp_send_json_success( [ 'message' => 'Datas updated' ], 200 );
-  }
-
-  public function get_checkout_user_datas()
-  {
-    $datas = [
-      'billing_email'      => WC()->customer->get_billing_email(),
-      'billing_first_name' => WC()->customer->get_billing_first_name(),
-      'billing_last_name'  => WC()->customer->get_billing_last_name(),
-      'billing_phone'      => WC()->customer->get_billing_phone(),
-      'billing_country'    => WC()->customer->get_billing_country(),
-      'billing_state'      => WC()->customer->get_billing_state(),
-      'billing_postcode'   => WC()->customer->get_billing_postcode(),
-      'billing_address_1'  => WC()->customer->get_billing_address_1(),
-      'shipping_method'    => WC()->session->get( 'chosen_shipping_methods' )[0],
-      'payment_method'     => WC()->session->get( 'chosen_payment_method' ),
-    ];
-
-    wp_send_json_success( $datas, 200 );
-  }
-
-  public function get_shipping_methods()
-  {
-    $valid_methods = [];
-
-    WC()->shipping->calculate_shipping( WC()->cart->get_shipping_packages() );
-
-    $shipping_methods = WC()->shipping->get_packages();
-
-    foreach( $shipping_methods[0]['rates'] as $id => $shipping_method ){
-      $valid_methods[] = [
-        'method_id' => $shipping_method->method_id,
-        'type'      => $shipping_method->method_id,
-        'id'        => $shipping_method->id,
-        'name'      => $shipping_method->label,
-        'price'     => $shipping_method->cost,
-        'taxes'     => $shipping_method->taxes,
-      ];
-    }
-  
-    if( !empty( $valid_methods ) ){
-      wp_send_json_success( $valid_methods, 200 );
-    }else{
-      wp_send_json_error( [ 'code' => 120, 'message' => 'No shipping methods.' ], 405 );
-    }
-  }
-
-  public function get_payment_methods()
-  {
-    if( !WC()->cart->is_empty() ){
-      $data = [];
-      
-      foreach( WC()->payment_gateways->get_available_payment_gateways() as $key => $gateway ){
-        $data[] = [
-          'id'                => $gateway->id,
-          'order_button_text' => $gateway->order_button_text,
-          'title'             => $gateway->get_title(),
-          'description'       => $gateway->get_description(),
-        ];
-      }
-
-      wp_send_json_success( $data, 200 );
-    }else{
-      wp_send_json_error( [ 'code' => 110, 'message' => 'Empty cart' ], 405 );
-    }
-  }
-
   public function get_cart_products()
   {
 
@@ -211,15 +90,15 @@ class Cart{
     if( ! WC()->cart->is_empty() ){
 
       $datas['hash']          = WC()->cart->get_cart_hash();
-      $datas['tax']           = WC()->cart->get_subtotal_tax();
-      $datas['shipping']      = WC()->cart->get_shipping_total();
+      $datas['tax']           = wc_price( WC()->cart->get_subtotal_tax() );
+      $datas['shipping']      = wc_price( WC()->cart->get_shipping_total() );
       
       if( $coupons = WC()->cart->get_coupons() ){
         $temp = [];
         foreach ( $coupons as $code => $coupon ) {
             $temp[$code] = [
               'code' => $coupon->get_code(),
-              'amount' => $coupon->get_amount(),
+              'amount' => wc_price( $coupon->get_amount() ),
               'description' => $coupon->get_description(),
             ];
         }
@@ -230,8 +109,8 @@ class Cart{
       }
 
       $datas['fees']          = WC()->cart->get_fees();
-      $datas['subtotal']      = WC()->cart->get_subtotal();
-      $datas['total']         = WC()->cart->get_total(false);
+      $datas['subtotal']      = wc_price( WC()->cart->get_subtotal() );
+      $datas['total']         = wc_price( WC()->cart->get_total(false) );
       $datas['content_count'] = WC()->cart->get_cart_contents_count();
 
       $items = WC()->cart->get_cart();
@@ -264,11 +143,6 @@ class Cart{
     }
 
     wp_send_json_error( [ 'code' => 104, 'message' => 'No Item Key' ], 405 );
-  }
-
-  public function get_countries()
-  {
-    wp_send_json_success( WC()->countries->get_countries(), 200 );
   }
 
   public function get_states()

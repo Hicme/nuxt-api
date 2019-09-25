ab03a3db2e9e51ec8e79579cae5eaeff35d5ec51
Oleg Podmarkov <oleg.podmarkov@prosvit.design>
2019-09-25 17:57:05 +0300

    search

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -2,14 +2,14 @@
 
 function nuxt_api()
 {
-    return \system\StartUp::instance();
+  return \system\StartUp::instance();
 }
 
 if( ! function_exists( 'render_input' ) ){
   function render_input( array $args )
   {
     if( empty( $args['id'] ) ){
-        return;
+      return;
     }
 
     $args['type'] = isset( $args['type'] ) ? $args['type'] : 'text';
@@ -23,45 +23,48 @@ if( ! function_exists( 'render_input' ) ){
     if ( ! empty( $args['attributes'] ) && is_array( $args['attributes'] ) ) {
 
     foreach ( $args['attributes'] as $attribute => $value ) {
-        $attributes[] = esc_attr( $attribute ) . '="' . esc_attr( $value ) . '"';
+      $attributes[] = esc_attr( $attribute ) . '="' . esc_attr( $value ) . '"';
     }
     }
 
     ?>
     <p class="input-field-wrapper input-<?php echo $args['id'] ?>">
     <label for="input_filed_<?php echo $args['id'] ?>">
-        <?php echo wp_kses_post( $args['label'] ); ?>
+      <?php echo wp_kses_post( $args['label'] ); ?>
     </label>
 
     <input type="<?php echo esc_attr( $args['type'] ); ?>" id="input_filed_<?php echo esc_attr( $args['id'] ); ?>" name="<?php echo esc_attr( $args['name'] ); ?>" class="<?php echo esc_attr( $args['class'] ); ?>" value="<?php echo esc_attr( $args['value'] ); ?>" <?php echo implode( ' ', $attributes ); ?> />
 
     <?php
-        if( !empty( $args['description'] ) ){
+      if( !empty( $args['description'] ) ){
         echo '<span class="description">'. wp_kses_post( $args['description'] ) .'</span>';
-        }
+      }
     ?>
     </p>
     <?php
   }
 }
 
-function get_template_html( $template_path, $args = array() )
-{
+if( ! function_exists( 'get_template_html' ) ){
+  function get_template_html( $template_path, $args = array() )
+  {
     if ( ! empty( $args ) && is_array( $args ) ) {
-		extract( $args );
+      extract( $args );
     }
     
     if ( ! file_exists( $template_path ) ) {
-        return false;
+      return false;
     }
 
     ob_start();
     include $template_path;
     return ob_get_clean();
+  }
 }
 
-function send_reset_password_email( $reset_key, $user )
-{
+if( ! function_exists( 'send_reset_password_email' ) ){
+  function send_reset_password_email( $reset_key, $user )
+  {
     $reset_link = network_site_url( 'login/reset?login=' . $user->user_login . "&key=$reset_key" );
     $subject = __( 'Request to reset password', 'nuxtapi' );
     $headers = [ 'content-type: text/html' ];
@@ -69,7 +72,13 @@ function send_reset_password_email( $reset_key, $user )
 
     $html = get_template_html( P_PATH . 'templates\email\html-reset-password.php', [ 'reset_link' => $reset_link, 'subject' => $subject ] );
 
-
     return wp_mail( $user->user_email, $subject, $html, $headers, $attachments );
+  }
+}
 
+if( ! function_exists( 'get_nuxt_permalink' ) ){
+  function get_nuxt_permalink()
+  {
+    
+  }
 }
--- includes/system/ajax.php
+++ includes/system/ajax.php
@@ -6,7 +6,54 @@ class Ajax{
 
   public function __construct()
   {
-
+    add_action( 'wp_ajax_getSearchResults', [ $this, 'initSearch' ] );
+    add_action( 'wp_ajax_nopriv_getSearchResults', [ $this, 'initSearch' ] );
   }
 
+  public function initSearch()
+  {
+    if ( isset( $_REQUEST['search'] ) ) {
+      global $wpdb;
+
+      $return      = [];
+      $search_word = sanitize_text_field( $_REQUEST['search'] );
+      $results     = $wpdb->get_results( "SELECT ID, post_title, post_content, post_type FROM {$wpdb->posts} WHERE post_type IN ('page', 'post', 'product') AND post_status = 'publish' AND ( post_title LIKE '%{$search_word}%' OR post_content LIKE '%{$search_word}%' )", OBJECT );
+
+      if ( $results ) {
+        foreach ( $results as $result ) {
+          $return[] = [
+            'ID'          => $result->ID,
+            'title'       => $this->highlight_text( $search_word, $result->post_title ),
+            'description' => $this->cut_highlight_text( $result->post_content, $search_word ),
+            'link'        => get_nuxt_permalink( $result->ID )
+          ];
+        }
+      }
+
+      wp_send_json_success( $return, 200 );
+    }
+
+    wp_send_json_error( [ 'message' => __( 'No search value.', 'nuxtapi' ) ], 405 );
+  }
+
+  private function highlight_text( $serch, $text )
+  {
+    $start_emp_token = '<span class="highlight_search">';
+    $end_emp_token = '</span>';
+
+    $content = preg_replace(
+      "/($serch)/iu",
+      $start_emp_token . '\\1' . $end_emp_token,
+      $text
+    );
+
+    return $content;
+  }
+
+  private function cut_highlight_text( $text, $search_word, $size = 40 )
+  {
+    $text = wp_strip_all_tags( $text );
+    $highlight = $this->highlight_text( $search_word, $text );
+    return $highlight;
+  }
 }
